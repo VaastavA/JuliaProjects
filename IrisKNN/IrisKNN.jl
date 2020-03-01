d49fb5246992ce45592a607b2debdb852eda1274
Vaastav Arora <arora74@purdue.edu>
2020-03-01 15:23:30 -0500

    Restructure

--- IrisKNN/IrisKNN.jl
+++ IrisKNN/IrisKNN.jl
@@ -5,17 +5,17 @@ iris = dataset("datasets","iris")
 
 p1 = plot(iris, x=:SepalLength, y=:PetalLength, color=:Species)
 
-img1 = PDF("iris_plot1.pdf", 6inch, 4inch)
+img1 = PDF("plots/iris_plot1.pdf", 6inch, 4inch)
 draw(img1, p1)
 
 p2 = plot(iris, x=:Species, y=:PetalLength, Geom.boxplot)
 
-img2 = PDF("iris_plot2.pdf", 6inch, 4inch)
+img2 = PDF("plots/iris_plot2.pdf", 6inch, 4inch)
 draw(img2,p2)
 
 p3 = plot(iris, x=:PetalWidth, y=:PetalLength, color=:Species)
 
-img3 = PDF("iris_plot3.pdf", 6inch, 4inch)
+img3 = PDF("plots/iris_plot3.pdf", 6inch, 4inch)
 draw(img3, p3)
 
 
