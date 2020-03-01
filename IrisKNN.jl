d525ec626f4c92348ba32c643642f0fda557dfe9
Vaastav Arora <arora74@purdue.edu>
2020-02-29 19:19:32 -0500

    Filter not working

--- IrisKNN.jl
+++ IrisKNN.jl
@@ -1,5 +1,19 @@
-import Pkg; Pkg.add(["RDatasets","PyPlot"])
+#import Pkg; Pkg.add(["RDatasets","PyPlot"])
 using RDatasets, PyPlot
 
 iris = dataset("datasets","iris")
+colors = ['b','g','r','c','m','y','k','w']
+species = Set()
+
+for s in iris.Species
+	push!(species,s)
+end
+
+species = string.(collect(species))
+println(species)
+tdf = filter(r -> in(r.Species, species[1]), iris)
+println(tdf)
+
+scatter(iris[:SepalLength], iris[:PetalLength])
+savefig("my_plot.pdf")
 describe(iris)
